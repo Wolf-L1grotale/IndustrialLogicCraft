39b4fff7e3ed627222aa2cddbbd366d840adb1e5
Admin <admin@local.lo>
2025-07-07 16:20:33 +0300

    Initial commit

---
+++ src/main/java/net/wolf_l1grotale/IndustrialLogicCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wolf_l1grotale;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IndustrialLogicCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
