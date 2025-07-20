8e539263c3737724c30fbdcd82233be32086a6b3
Admin <admin@local.lo>
2025-07-20 15:23:38 +0300

    Исправление у ориентации блока при установке в мир

--- src/main/java/net/wolf_l1grotale/industriallogiccraft/block/custom/SolidFuelGeneratorBlock.java
+++ src/main/java/net/wolf_l1grotale/industriallogiccraft/block/custom/SolidFuelGeneratorBlock.java
@@ -1,28 +1,37 @@
 package net.wolf_l1grotale.industriallogiccraft.block.custom;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.wolf_l1grotale.industriallogiccraft.block.entity.ModBlockEntities;
-import net.wolf_l1grotale.industriallogiccraft.block.entity.custom.GrowthChamberBlockEntity;
 import net.wolf_l1grotale.industriallogiccraft.block.entity.custom.SolidFuelGeneratorBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
+
 public class SolidFuelGeneratorBlock extends BlockWithEntity implements BlockEntityProvider {
     public static final MapCodec<SolidFuelGeneratorBlock> CODEC = SolidFuelGeneratorBlock.createCodec(SolidFuelGeneratorBlock::new);
+
+    public static final Property<Direction> FACING = Properties.HORIZONTAL_FACING;
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
 
 
     public SolidFuelGeneratorBlock(Settings settings) {
@@ -43,6 +52,11 @@ public class SolidFuelGeneratorBlock extends BlockWithEntity implements BlockEnt
     @Override
     protected BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite());
     }
 
     @Override
